3e2e5899313dbceb2273e3b3b1b2c88caec70efe
mazlumerceylan <mazlumerceylan98@gmail.com>
2023-05-16 00:36:58 +0200

    form created

--- index.php
+++ index.php
@@ -7,6 +7,19 @@
   <title>Censura Parole</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"></head>
 <body>
-  
+<div class="container">
+        <h1 class="mt-5">Censura Parole</h1>
+        <form action="process.php" method="post">
+            <div class="form-group">
+                <label for="paragrafo">Scrivi un paragrafo:</label>
+                <textarea class="form-control" id="paragrafo" name="paragrafo" rows="3"></textarea>
+            </div>
+            <div class="form-group">
+                <label for="badword">Scrivi la parola da censurare:</label>
+                <input type="text" class="form-control" id="badword" name="badword">
+            </div>
+            <button type="submit" class="btn btn-primary">Invia</button>
+        </form>
+    </div>
 </body>
 </html>
